d28a0981d1c05b43e26f44fd2d00b6c96fae5f87
sleepkqq <nikita32ivanov@gmail.com>
2025-04-04 21:11:45 +0300

    add notification events

--- src/main/java/com/sleepkqq/sololeveling/avro/constants/KafkaGroupIds.java
+++ src/main/java/com/sleepkqq/sololeveling/avro/constants/KafkaGroupIds.java
@@ -3,7 +3,11 @@ package com.sleepkqq.sololeveling.avro.constants;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
+@SuppressWarnings("unused")
 public class KafkaGroupIds {
 
-  public static final String TASK_GROUP_ID = "task-group";
+  public static final String AI_GROUP_ID = "ai-group";
+  public static final String PLAYER_GROUP_ID = "player-group";
+  public static final String UI_GROUP_ID = "ui-group";
+  public static final String NOTIFICATION_GROUP_ID = "notification-group";
 }
--- src/main/java/com/sleepkqq/sololeveling/avro/constants/KafkaTaskTopics.java
+++ src/main/java/com/sleepkqq/sololeveling/avro/constants/KafkaTaskTopics.java
@@ -3,8 +3,12 @@ package com.sleepkqq.sololeveling.avro.constants;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
+@SuppressWarnings("unused")
 public class KafkaTaskTopics {
 
   public static final String SAVE_TASKS_TOPIC = "save-tasks";
   public static final String GENERATE_TASKS_TOPIC = "generate-tasks";
+  public static final String SEND_NOTIFICATION_TOPIC = "send-notification";
+  public static final String TG_NOTIFICATION_TOPIC = "tg-notification";
+  public static final String UI_NOTIFICATION_TOPIC = "ui-notification";
 }
